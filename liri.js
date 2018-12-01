e061d7ad5e8fd37da61bf0f9a026e2b609724f90
Chad Cook <chadcookcoding@gmail.com>
2018-12-01 07:29:57 -0500

    started song search.

--- liri.js
+++ liri.js
@@ -1,6 +1,10 @@
 require('dotenv').config()
 
-const spotify = require('node-spotify-api')
+const spotifyPackage = require('node-spotify-api')
+const spotify = new spotifyPackage({
+  id: process.env.SPOTIFY_ID,
+  secret: process.env.SPOTIFY_SECRET
+})
 const axios = require('axios')
 const moment = require('moment')
 
@@ -26,11 +30,32 @@ switch (command) {
 
 
 function concert(search) {
+  if (search) {
+    axios
+      .get("https://rest.bandsintown.com/artists/" + search + "/events?app_id=codingbootcamp")
+      .then(function(res) {
+        for ( let i = 0; i < 10; i++ ) {
+          // console log the info for each event
+        }
+
+        // console.log( res.data[0])
+        console.log( res.data[0].venue.name )
+        console.log( res.data[0].venue.city + ", " + res.data[0].venue.region )
+        console.log( moment(res.data[0].datetime).format('MM-DD-YYYY') )
+      })
+  } else {
+    console.log('You did not search for anything')
+  }
+
 
 }
 
 function song(search) {
-
+  if (search) {
+    
+  } else {
+    console.log('You did not search for anything')
+  }
 }
 
 function movie(search) {
